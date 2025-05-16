e38acce2985caa8ec95ecdbec9ea6acd7dd55c72
its-mr-gray <ferrell_gray3@proton.me>
2025-05-15 20:13:50 -0400

    add extra list commands, add update task command

--- task-tracker-cli/main.py
+++ task-tracker-cli/main.py
@@ -1,12 +1,12 @@
 """
 The application should run from the command line, accept user actions and inputs as arguments, and store the tasks in a JSON file. The user should be able to:
 
-    Add, Update, and Delete tasks
-    Mark a task as in progress or done
+    Add, Update, and Delete tasks <- DONE
+    Mark a task as in progress or done <- DONE
     List all tasks <- DONE
-    List all tasks that are done
-    List all tasks that are not done
-    List all tasks that are in progress
+    List all tasks that are done <- DONE
+    List all tasks that are not done <- DONE
+    List all tasks that are in progress <- DONE
 
 Here are some constraints to guide the implementation:
 
@@ -24,7 +24,7 @@ import json
 import os
 
 json_file = "tasklist.json"
-default_data = {"TODO": [], "IN PROGRESS": [], "DONE": []}
+default_data: dict = {"TODO": [], "IN PROGRESS": [], "DONE": []}
 
 if not os.path.exists(json_file):
     with open(json_file, "w") as f:
@@ -32,36 +32,86 @@ if not os.path.exists(json_file):
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest="command", required=True)
-list_task_parser = subparsers.add_parser("list", help="list all tasks in the tracker.")
+list_all_tasks_parser = subparsers.add_parser(
+    "list-all", help="list all tasks in the tracker."
+)
+list_unfinished_tasks_parser = subparsers.add_parser(
+    "list-unfinished", help="list all unfinished tasks in the tracker."
+)
+list_inprog_tasks_parser = subparsers.add_parser(
+    "list-in-progress", help="list all in progress tasks in the tracker."
+)
+list_finished_tasks_parser = subparsers.add_parser(
+    "list-done", help="list all finished tasks in the tracker."
+)
 add_task_parser = subparsers.add_parser("add", help="add a new task.")
-update_task_parser = subparsers.add_parser("update", help="update a task.")
+update_task_parser = subparsers.add_parser(
+    "update-status", help="update a task's status."
+)
 remove_task_parser = subparsers.add_parser("remove", help="remove a task.")
 args = parser.parse_args()
 
 with open(json_file, "r+") as f:
     data = json.load(f)
 
-    if args.command == "list":
+    if args.command == "list-all":
         print(json.dumps(data, indent=2))
+
+    elif args.command == "list-unfinished":
+        todo_vals = [v for v in data["TODO"]]
+        in_prog_vals = [v for v in data["IN PROGRESS"]]
+        print(todo_vals + in_prog_vals)
+
+    elif args.command == "list-done":
+        print(json.dumps(data["DONE"], indent=2))
+
+    elif args.command == "list-in-progress":
+        print(json.dumps(data["IN PROGRESS"], indent=2))
+
     elif args.command == "add":
-        new_task = input("what would you like to add?\n")
+        new_task = input("what would you like to add?\n").upper()
         data["TODO"].append(new_task)
         f.seek(0)
         json.dump(data, f, indent=2)
         f.truncate()
-    elif args.command == "update":
-        update_task = input("what task needs updating?\n")
-        status_query = input(
-            "does the status of this task need to be updated? Y/N\n"
+
+    elif args.command == "update-status":
+        todo_contents = [task for task in data["TODO"]]
+        in_prog_contents = [task for task in data["IN PROGRESS"]]
+        done_contents = [task for task in data["DONE"]]
+        task_to_update = input(
+            f"what task would you like to change the status of?\n {data.values()}"
         ).upper()
-        if status_query == "Y":
-            update_status = input("what is the updated status of this task?\n")
-            # update status logic
-        elif status_query == "N":
-            updated_task = input("please input the update for this task\n")
-            # update task logic
+        if task_to_update in todo_contents:
+            todo_contents.remove(task_to_update)
+        elif task_to_update in in_prog_contents:
+            in_prog_contents.remove(task_to_update)
+        elif task_to_update in done_contents:
+            done_contents.remove(task_to_update)
+        status_to_update = input(
+            f"what status should this task now have?\n{data.keys()}"
+        ).upper()
+
+        if status_to_update == "TODO":
+            todo_contents.append(task_to_update)
+        elif status_to_update == "IN PROGRESS":
+            in_prog_contents.append(task_to_update)
+        elif status_to_update == "DONE":
+            done_contents.append(task_to_update)
+        else:
+            print("invalid status selection")
+
+        data["TODO"] = list(todo_contents)
+        data["IN PROGRESS"] = list(in_prog_contents)
+        data["DONE"] = list(done_contents)
+        if status_to_update not in data.keys():
+            print("invalid status selection")
+        f.seek(0)
+        json.dump(data, f, indent=2)
+        f.truncate()
+
     elif args.command == "remove":
-        remove_task = input("which task would you like to remove?\n")
+        remove_task = input("which task would you like to remove?\n").upper()
         data["TODO"] = [task for task in data["TODO"] if task != remove_task]
         f.seek(0)
         json.dump(data, f, indent=2)
