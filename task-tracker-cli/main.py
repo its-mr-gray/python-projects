da3eb2f3c568bee17a036a8de09f428d60013da2
its-mr-gray <ferrell_gray3@proton.me>
2025-05-16 20:29:12 -0400

    remove docstring

--- task-tracker-cli/main.py
+++ task-tracker-cli/main.py
@@ -1,24 +1,3 @@
-"""
-The application should run from the command line, accept user actions and inputs as arguments, and store the tasks in a JSON file. The user should be able to:
-
-    Add, Update, and Delete tasks <- DONE
-    Mark a task as in progress or done <- DONE
-    List all tasks <- DONE
-    List all tasks that are done <- DONE
-    List all tasks that are not done <- DONE
-    List all tasks that are in progress <- DONE
-
-Here are some constraints to guide the implementation:
-
-    Use positional arguments in command line to accept user inputs. <- DONE
-    Use a JSON file to store the tasks in the current directory. <- DONE
-    The JSON file should be created if it does not exist. <- DONE
-    Use the native file system module of your programming language to interact with the JSON file. <- Done
-    Do not use any external libraries or frameworks to build this project. <- DONE
-    Ensure to handle errors and edge cases gracefully.
-
-"""
-
 from typing import Any
 
 import utilities
